c84c9e755edd815008e1f8bceded1f39c7962357
Dolly <dollyyadhiramolloch@gmail.com>
2023-07-09 18:31:14 -0500

    Mi proyecto DG

--- src/config/settings.py
+++ src/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u4$obz&^lfbaz+#f-*m7fye-uj=2t*(0ux28ra532h(lo^_k+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'phytonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '.pythonanywhere.com']
 
 
 # Application definition
